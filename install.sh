4502afa131a7b4d7dc25e587de47ef1e3ce9997b
Josh Black-Star <josh@Joshs-MacBook-Pro.local>
2023-09-16 00:46:32 -0700

    lots of stuff

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# create symbolic links required for proper system function
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
+# check if homebrew is installed, otherwise install it
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# update homebrew
+brew update
+
+# install everything in the Brewfile
+brew bundle --file ~/.dotfiles/Brewfile
+
+# install the macosdefaults.sh script
+source ~/.dotfiles/macosdefaults.sh
