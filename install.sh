8383a6135c0d9e5efe8d380cfc34f3dff5c242c0
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2024-06-24 14:43:08 -0700

    big updates to the install.sh script, testing after a full system refresh

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-# create symbolic links required for proper system function
-for file in .gitconfig .gitignore_global .zshrc Brewfile; do
-  if [ -f ~/.dotfiles/$file ]; then
-    ln -sf ~/.dotfiles/$file ~/$file
+# Function to create and verify symbolic links
+create_and_verify_symlink() {
+  local source_file=$1
+  local target_file=$2
+
+  if [ -f "$source_file" ]; then
+    ln -sf "$source_file" "$target_file"
+    if [ "$(readlink "$target_file")" == "$source_file" ]; then
+      echo "Successfully linked $target_file to $source_file"
+    else
+      echo "Failed to link $target_file to $source_file"
+    fi
   else
-    echo "$file not found!"
-    exit 1
+    echo "$source_file not found!"
   fi
-done
+}
+
+# create symbolic links required for proper system function
+create_and_verify_symlink ~/.dotfiles/git/.gitconfig ~/.gitconfig
+create_and_verify_symlink ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
+create_and_verify_symlink ~/.dotfiles/config/.zshrc ~/.zshrc
+create_and_verify_symlink ~/.dotfiles/config/Brewfile ~/Brewfile
 
 # check if homebrew is installed, otherwise install it
 if test ! $(which brew); then
@@ -20,14 +33,22 @@ fi
 
 # if the first argument is not --no-brew, install from Brewfile
 if [ "${1:-}" != "--no-brew" ]; then
-  # install everything in the Brewfile
-  brew bundle --file ~/.dotfiles/Brewfile
+  if [ -f ~/.dotfiles/config/Brewfile ]; then
+    # install everything in the Brewfile
+    brew bundle --file ~/.dotfiles/config/Brewfile
+  else
+    echo "Brewfile not found in ~/.dotfiles!"
+  fi
 fi
 
-# install the macosdefaults.sh script
-if [ -f ~/.dotfiles/.macos ]; then
-  source ~/.dotfiles/.macos
+# Execute the .macos script
+if [ -f ~/.dotfiles/config/.macos ]; then
+  echo "Executing ~/.dotfiles/config/.macos"
+  chmod +x ~/.dotfiles/config/.macos
+  ~/.dotfiles/config/.macos
+  echo "Finished executing ~/.dotfiles/config/.macos"
+
+  killall Dock
 else
-  echo "~/.dotfiles/.macos not found!"
-  exit 1
+  echo "~/.dotfiles/config/.macos not found!"
 fi
